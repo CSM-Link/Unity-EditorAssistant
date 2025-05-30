14898e49c4eb2837a58bbcb067a1a4ad740014ac
CSM <924466726@qq.com>
2025-05-30 09:27:37 +0800

    no message

---
+++ Editor/EditorQuickOptions.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEngine;
+
+public class EditorQuickOptions : MonoBehaviour
+{
+    [MenuItem("Tools/Toggle Domain and Scene Reloading")]
+    public static void DisableReloading()
+    {
+        var enterPlayModeOption = EditorSettings.enterPlayModeOptions;
+        if (enterPlayModeOption == EnterPlayModeOptions.None)
+        {
+            EditorSettings.enterPlayModeOptions = EnterPlayModeOptions.DisableDomainReload | EnterPlayModeOptions.DisableSceneReload;
+            Debug.Log("Enter Play Mode Settings Updated: Domain/Scene Reloading Disabled");
+        }
+        else
+        {
+            EditorSettings.enterPlayModeOptions = EnterPlayModeOptions.None;
+            Debug.Log("Enter Play Mode Settings Updated: Domain/Scene Reloading Enabled");
+        }
+        AssetDatabase.SaveAssets();
+    }
+
+}
